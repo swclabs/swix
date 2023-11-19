7d45d0d0501e0440792d5a80f6a916971b2a4320
kawasaki <iduchungho@gmail.com>
2023-11-19 19:00:28 +0700

    db: init schema.sql

--- internal/model/users.go
+++ internal/model/users.go
@@ -1,7 +1,7 @@
 package model
 
 type User struct {
-	UserID      int64  `json:"user_id" gorm:"user_id"`
+	UserID      int64  `json:"id" gorm:"id"`
 	Email       string `json:"email" gorm:"column:email"`
 	PhoneNumber string `json:"phone_number" gorm:"column:phone_number"`
 	FirstName   string `json:"first_name" gorm:"column:first_name"`
