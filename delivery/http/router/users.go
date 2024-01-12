1ef00dd86d06cf72af22a5c52d7ed6f2f8dc08ea
kawasaki <iduchungho@gmail.com>
2024-01-12 08:22:25 +0700

    chore: change controller2 to controller

--- delivery/http/router/users.go
+++ delivery/http/router/users.go
@@ -2,25 +2,25 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
-	controller2 "swclabs/swiftcart/delivery/http/controller"
+	"swclabs/swiftcart/delivery/http/controller"
 	"swclabs/swiftcart/delivery/http/middleware"
 )
 
 func Users(e *gin.Engine) {
 	user := e.Group("/v1/users")
-	user.GET("/", middleware.SessionProtected, controller2.GetMe)
-	user.PUT("/", controller2.UpdateUserInfo)
-	user.POST("/image", middleware.SessionProtected, controller2.UpdateUserImage)
+	user.GET("/", middleware.SessionProtected, controller.GetMe)
+	user.PUT("/", controller.UpdateUserInfo)
+	user.POST("/image", middleware.SessionProtected, controller.UpdateUserImage)
 }
 
 func Auth(e *gin.Engine) {
 	auth := e.Group("/v1/auth")
-	auth.POST("/signup", controller2.SignUp)
-	auth.POST("/login", controller2.Login)
-	auth.GET("/logout", controller2.Logout)
+	auth.POST("/signup", controller.SignUp)
+	auth.POST("/login", controller.Login)
+	auth.GET("/logout", controller.Logout)
 }
 
 func OAuth2(e *gin.Engine) {
 	auth0 := e.Group("/v1/oauth2")
-	auth0.GET("/login", controller2.Auth0Login)
+	auth0.GET("/login", controller.Auth0Login)
 }
