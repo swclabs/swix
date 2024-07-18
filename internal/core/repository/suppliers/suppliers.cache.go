98d2ff504b77d373cfcd1fd9f634d8acdc470beb
anthonyynguyen <anthony2742000@gmail.com>
2024-07-18 13:33:44 +0700

    Develop Backend 'updateSuppliers' to Edit suppliers

--- internal/core/repository/suppliers/suppliers.cache.go
+++ internal/core/repository/suppliers/suppliers.cache.go
@@ -41,6 +41,6 @@ func (c *_cache) InsertAddress(ctx context.Context, addr entity.SuppliersAddress
 	return c.supplier.InsertAddress(ctx, addr)
 }
 
-func (c *cache) Edit(ctx context.Context, sup domain.Suppliers) error {
+func (c *_cache) Edit(ctx context.Context, sup entity.Suppliers) error {
 	return c.supplier.Edit(ctx, sup)
 }
