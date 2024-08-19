287a4b21098547c1e21f75b2b0c67c378ed0d3f1
anthonyynguyen <anthony2742000@gmail.com>
2024-08-19 16:45:23 +0700

    Develop /core/service/classify

--- internal/core/domain/entity/categories.go
+++ internal/core/domain/entity/categories.go
@@ -3,7 +3,7 @@ package entity
 
 // Categories Table
 type Categories struct {
-	ID          string `json:"id" db:"id"`
+	ID          int64  `json:"id" db:"id"`
 	Name        string `json:"name" db:"name" validate:"required"`
 	Description string `json:"description" db:"description" validate:"required"`
 }
